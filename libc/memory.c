c0ca77cf2418ba2e8387e6194d3c13fc2a27c78f
Rubén Aciego Casero <rubenaciego@hotmail.es>
2018-09-06 17:55:43 +0200

    Update memory.c

--- libc/memory.c
+++ libc/memory.c
@@ -11,15 +11,6 @@ void memset(byte* dest, byte val, int len)
     for (; len != 0; len--) *dest++ = val;
 }
 
-void meminit()
-{
-    struct mem_data* start = (struct mem_data*)MEM_START;
-    start->prev_chunck = NULL;
-    start->next_chunck = NULL;
-    start->chunck_size = 0;
-    start->next_chunck_free = 1;
-}
-
 u32 current_mem = 0x10000;
 
 void* kmalloc(u32 size)
