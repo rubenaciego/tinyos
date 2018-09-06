6bb02895d599be701f20807d0303261ae7d228fe
Rubén Aciego Casero <rubenaciego@hotmail.es>
2018-09-06 17:56:09 +0200

    Update memory.h

--- include/memory.h
+++ include/memory.h
@@ -1,7 +1,5 @@
 #include <types.h>
 
-void meminit();
 void memcpy(byte* source, byte* dest, int nbytes);
 void memset(byte* dest, byte val, int len);
 void* kmalloc(u32 size);
-void kfree(u32* addr);
